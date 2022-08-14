60aa513f457965946e2ce4f8f896b1310de4a910
yellyoshua <usuario1529yoshua@hotmail.com>
2022-08-14 12:59:32 -0500

    fix topbar menu

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,7 +7,7 @@ export default function ProfileComponent({session}) {
       href={`https://twitter.com/${session.username}`}
     >
       <img
-        src={session.profile_image_url}
+        src={session.profile_image_url || "https://via.placeholder.com/150"}
         alt={session.username}
         className='rounded-full mr-2 border-2 border-blue-500 h-12 w-12'
       />
--- src/ui/TopBar.jsx
+++ src/ui/TopBar.jsx
@@ -8,7 +8,7 @@ const LogoutWithSession = withSession(LogoutComponent);
 export default function TopBar({appTitle = "Tweets Deploy"}) {
   return (
     <div className="flex items-center w-full justify-between mt-10 mb-3">
-      <h1 className='text-lg font-bold font-mono'>{appTitle}</h1>
+      <h1 className='text-lg font-bold font-mono whitespace-nowrap'>{appTitle}</h1>
       <div className="flex items-center w-full justify-end">
         <ProfileWithSession />
         <LogoutWithSession />
